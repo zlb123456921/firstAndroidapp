d8133ba64f5effb17673c15aefcb54261a59502d
zlb123456921 <1498807162@qq.com>
2020-03-22 03:24:19 +0800

    温度转换器

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,34 +1,64 @@
 package com.example.myapplication;
 
+//import  android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import java.lang.*;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager2.widget.ViewPager2;
 
+import android.preference.EditTextPreference;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements  View.OnClickListener{
 
-    @Override
+    EditText a;
+    TextView t;
+    Button b;
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        a=(EditText) findViewById(R.id.editText);
+        b=(Button) findViewById(R.id.button);
+        t=(TextView)findViewById(R.id.textView2);
+        b.setOnClickListener(this);
+//        b.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public  void onClick(View v) {
+//                Log.i("main", "onClick called");
+//                String a1 = a.getText().toString();
+//                Double a2 = Double.parseDouble(a1);
+//                a2 = a2 * 1.8 + 32;
+//                t.setText("华氏度："+a2);
+//            }
+//
+//        });
+
+    }
+
+
+    @Override
+    public  void onClick(View v) {
+        Log.i("main", "onClick called");
+        String a1 = a.getText().toString();
+        Double a2 = Double.parseDouble(a1);
+        a2 = a2 * 1.8 + 32;
+        t.setText("华氏度："+a2);
     }
 
     @Override
@@ -52,4 +82,6 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
